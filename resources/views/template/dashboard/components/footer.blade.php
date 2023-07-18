659487ce2af70c73bf9102e0b4606e5b5c1160fa
git-yogie <yogie.prayoga35@gmail.com>
2023-07-18 12:55:43 +0800

    new commint

---
+++ resources/views/template/dashboard/components/footer.blade.php
@@ -0,0 +1,11 @@
+<footer style="bottom: 5px">
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2023 &copy; yogieDesk</p>
+        </div>
+        <div class="float-end">
+            <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                    href="https://saugi.me">Yogie</a></p>
+        </div>
+    </div>
+</footer>
